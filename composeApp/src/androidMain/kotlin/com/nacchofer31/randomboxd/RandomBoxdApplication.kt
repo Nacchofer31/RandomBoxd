565d4c7f38edfd90016563e6fe06c9e83e97710e
Nacchofer31 <10453558+Nacchofer31@users.noreply.github.com>
2026-06-04 13:14:07 +0200

    feat(android): integrate App Functions with Koin
    - Implement AppFunctionConfiguration.Provider
    - Add RandomBoxdAppFunctions to Koin module
    - Provide AppFunctions factory using Koin

--- composeApp/src/androidMain/kotlin/com/nacchofer31/randomboxd/RandomBoxdApplication.kt
+++ composeApp/src/androidMain/kotlin/com/nacchofer31/randomboxd/RandomBoxdApplication.kt
@@ -1,11 +1,16 @@
 package com.nacchofer31.randomboxd
 
 import android.app.Application
+import androidx.appfunctions.service.AppFunctionConfiguration
+import com.nacchofer31.randomboxd.appfunctions.RandomBoxdAppFunctions
 import com.nacchofer31.randomboxd.di.initKoin
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.GlobalContext
 
-class RandomBoxdApplication : Application() {
+class RandomBoxdApplication :
+    Application(),
+    AppFunctionConfiguration.Provider {
     override fun onCreate() {
         super.onCreate()
         initKoin {
@@ -13,4 +18,12 @@ class RandomBoxdApplication : Application() {
             androidContext(this@RandomBoxdApplication)
         }
     }
+
+    override val appFunctionConfiguration: AppFunctionConfiguration
+        get() =
+            AppFunctionConfiguration
+                .Builder()
+                .addEnclosingClassFactory(RandomBoxdAppFunctions::class.java) {
+                    GlobalContext.get().get<RandomBoxdAppFunctions>()
+                }.build()
 }
--- composeApp/src/androidMain/kotlin/com/nacchofer31/randomboxd/di/Modules.android.kt
+++ composeApp/src/androidMain/kotlin/com/nacchofer31/randomboxd/di/Modules.android.kt
@@ -1,11 +1,13 @@
 package com.nacchofer31.randomboxd.di
 
+import com.nacchofer31.randomboxd.appfunctions.RandomBoxdAppFunctions
 import com.nacchofer31.randomboxd.core.data.OnboardingPreferences
 import com.nacchofer31.randomboxd.core.data.UsernameDatabase
 import com.nacchofer31.randomboxd.database.getUserNameDatabase
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.okhttp.OkHttp
 import org.koin.core.module.Module
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 actual val platformModule: Module
@@ -14,4 +16,5 @@ actual val platformModule: Module
             single<HttpClientEngine> { OkHttp.create() }
             single<UsernameDatabase> { getUserNameDatabase(get()) }
             single { OnboardingPreferences(get()) }
+            singleOf(::RandomBoxdAppFunctions)
         }
