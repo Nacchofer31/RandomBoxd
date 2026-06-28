3ed65aafbed090a6a1494ae087daa42368f5cd12
Nacchofer31 <10453558+Nacchofer31@users.noreply.github.com>
2026-06-28 12:44:53 +0200

    chore: upgraded In App Review business logic

--- composeApp/src/androidMain/kotlin/com/nacchofer31/randomboxd/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/nacchofer31/randomboxd/MainActivity.kt
@@ -1,39 +1,24 @@
 package com.nacchofer31.randomboxd
 
-import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.google.android.play.core.review.ReviewManagerFactory
 import com.nacchofer31.randomboxd.app.RandomBoxdApp
+import com.nacchofer31.randomboxd.random_film.data.repository_impl.InAppReviewRepositoryImplAndroid
+import org.koin.android.ext.android.inject
 
 class MainActivity : ComponentActivity() {
+    private val inAppReviewRepository: InAppReviewRepositoryImplAndroid by inject()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
+        inAppReviewRepository.setActivity(this)
         setContent {
             RandomBoxdApp()
-        }
-        requestReviewIfNotShown()
-    }
-
-    private fun requestReviewIfNotShown() {
-        val prefs = getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
-        val hasShownReview = prefs.getBoolean("has_shown_review", false)
-        
-        if (!hasShownReview) {
-            val manager = ReviewManagerFactory.create(this)
-            val request = manager.requestReviewFlow()
-            request.addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    prefs.edit().putBoolean("has_shown_review", true).apply()
-                    val reviewInfo = task.result
-                    manager.launchReviewFlow(this, reviewInfo)
-                }
-            }
         }
     }
 }
